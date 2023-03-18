2e21c34e3e018d7bc4f4200941e5b87b541ead1e
James Stolhammer <jimstolhammer@gmail.com>
2023-03-17 21:56:41 -0700

    Removed some extra reads to bs4 in data lookups

--- src/models/items.py
+++ src/models/items.py
@@ -11,13 +11,14 @@ class ItemModel(QAbstractTableModel):
     def __init__(self, parent=None, xml=None):
         super().__init__(parent)
         self.xml = xml
+        self.items = xml('item')
 
     def headerData(self, section, orientation, role):
         if role == Qt.ItemDataRole.DisplayRole:
             if str(orientation) == 'Orientation.Horizontal':
                 return self.fields[section]
             elif str(orientation) == 'Orientation.Vertical' and section < self.rowCount():
-                return self.xml('item')[section].templateName.string
+                return self.items[section].templateName.string
         return None
 
     def rowCount(self, parent=None):
@@ -28,17 +29,16 @@ class ItemModel(QAbstractTableModel):
 
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
         if role == Qt.ItemDataRole.DisplayRole:
-            item = self.xml('item')[index.row()]
+            item = self.items[index.row()]
             field = self.fields[index.column()]
             try:
                 return str(item(field)[0].string)
             except IndexError:
                 return 'undefined'
-        return None
 
     def setData(self, index, value, role=Qt.ItemDataRole.EditRole):
         if (role == Qt.ItemDataRole.EditRole) and (value != ''):
-            item = self.xml('item')[index.row()]
+            item = self.items[index.row()]
             field = self.fields[index.column()]
             item(field)[0].string = str(value)
             return True
@@ -57,10 +57,11 @@ class ItemModel(QAbstractTableModel):
     def removeRows(self, row, count, parent):
         self.beginRemoveRows(parent, row, row + count)
         for i in range(count):
-            item = self.xml('item')[row]
+            item = self.items[row]
             item.decompose()
         self.endRemoveRows()
         self.layoutChanged.emit()
+        self.items = self.xml('item')
         return True
 
     def currentItems(self):
@@ -73,6 +74,7 @@ class ItemModel(QAbstractTableModel):
                 tag = soup(f'<item><templateName>{value}</templateName><slot>0</slot><quantity>1</quantity><uid>{str(uuid4())}</uid></item>', 'lxml-xml')
                 self.xml.append(tag.item)
         self.endInsertRows()
+        self.items = self.xml('item')
         self.layoutChanged.emit()
 
     def dropMimeData(self, data, action ,row, column, parent):
@@ -99,7 +101,4 @@ class ItemModel(QAbstractTableModel):
             for data_item in data_items:
                 text = data_item[Qt.ItemDataRole.DisplayRole]
                 self.insertRows(self.rowCount(), 1, text.value())
-        else:
-            #print(data.formats())
-            pass
         return True
